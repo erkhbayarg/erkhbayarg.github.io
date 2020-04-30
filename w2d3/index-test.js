79a43b437379209a6a43ca3548e5e9c4ea67d06a
erkhbayarg <erkhee16@gmail.com>
2020-04-30 00:28:26 -0500

    a

--- w2d3/index-test.js
+++ w2d3/index-test.js
@@ -2,12 +2,26 @@
 
 (function() {
     "use strict";
-	describe("Mocha walkthrough", () => {
+	describe("Mocha through", () => {
 		
 		describe('reverseArray ', () => {
-			it("returns the string reverseArray", () => {
+			it("returns the reverseArray", () => {
 				expect(reverseArray(["A", "B", "C"])).toEqual(["C", "B", "A"]);
 			});
 		}); 
-	});	
+		
+		describe('arrayToList ', () => {
+			it("returns the arrayToList", () => {
+				expect(arrayToList([10, 20])).toEqual({value: 10, rest: {value: 20, rest: null}});
+			});
+		}); 
+
+		describe('nth ', () => {
+			it("returns the nth", () => {
+				expect(nth({value: 10, rest: {value: 20, rest: null}}, 1)).toEqual(20);
+			});
+		}); 		
+		
+	});		
+	
 }());
