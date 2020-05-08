4fa43188490525b8822f744ad414e54d6cd8517b
erkhbayarg <erkhee16@gmail.com>
2020-05-08 00:06:54 -0500

    w3d4

--- w3d4/circle.js
+++ w3d4/circle.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-	
+	$('#btn1').on('click', function(){	
+		const width = $('#width').val();		
+		const amount = $('#amount').val();		
+		const growRate = $('#growRate').val();		
+		const numCircle = $('#numCircle').val();		
+
+		let circles = [];
+		for (let i = 0; i < numCircle; i++) {
+			circles.push( $("<div>", {class: "circle"}));
+		}
+
+		$("#content").append(...circles);
+
+		$('.circle').click(function() {
+			$(this).remove();
+		});
+
+		setInterval(() => $('.circle').css('width', '+='+amount), growRate);
+    setInterval(() => $('.circle').css('height', '+='+amount), growRate);
+	});
 });
